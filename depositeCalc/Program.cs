fd03f395067a389d84038e1812e654c10d663a2a
pavel <pavel_ts_pavlov@outlook.com>
2021-09-19 21:00:10 +0300

    first commit

---
+++ depositeCalc/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace depositeCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input the ammount");
+            double deposit = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Input the deposit period");
+            int period = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input the yearly interest");
+            double interestRate = double.Parse(Console.ReadLine());
+            double interestAmmount = deposit / interestRate*100;
+            double monthlyInterest = interestAmmount / 12;
+            double ammount = deposit + period * monthlyInterest;
+            Console.WriteLine(ammount);
+        }
+    }
+}
+
+
+
+//percentage = (yourNumber / totalNumber) * 100;
