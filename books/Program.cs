aff5e50266f8cddc44fd3814bedde7d24d6e4ed4
pavel <pavel_ts_pavlov@outlook.com>
2021-09-28 21:32:17 +0300

    task4+task5

---
+++ books/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace books
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input the number of pages in a book");
+            int pages = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input the pages per hour");
+            int pagesPerHour = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input the days to finish the book");
+            int days = int.Parse(Console.ReadLine());
+
+            int totalTime = pages / pagesPerHour;
+
+
+            int result= totalTime/days;
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ schoolSupplies/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace schoolSupplies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+
+            Console.WriteLine("Please isert number of pens you want:");
+            int penPackages = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please isert number of sharpies you want:");
+            int numberOfSharpys = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please isert the litres of cleaner you want:");
+            int LitresOfCleaner = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please isert the discount percent:");
+            int discountPercent = int.Parse(Console.ReadLine());
+
+            double pricePens = penPackages  * 5.8;
+            double priceSharpy = numberOfSharpys * 7.2;
+            double priceCleaner = LitresOfCleaner * 1.2;
+            double price1 = priceCleaner + pricePens + priceSharpy;
+            double discount = discountPercent / 100.00;
+            double price = discount * price1;
+            double finalPrice = price1 - price;
+            Console.WriteLine(finalPrice);
+        }
+    }
+}
