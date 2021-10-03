658faf38a73626523b1dcc510ac48fbb92e07e7f
pavel <pavel_ts_pavlov@outlook.com>
2021-10-03 20:19:41 +0300

    basketball task

---
+++ basketballEquipment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace basketballEquipment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please isert the yearly fee:");
+            int yearly = int.Parse(Console.ReadLine());
+
+            double sneakers = yearly * 0.6;
+            double outfit = sneakers * 0.8;
+            double ball = outfit * 0.25;
+            double accessories = ball * 0.2;
+
+            double expenses = sneakers + outfit + ball + accessories + yearly;
+            Console.WriteLine(expenses);
+
+        }
+    }
+}
