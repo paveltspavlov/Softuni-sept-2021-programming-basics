b0a0207a7bef6f979ea268215f3d3df6c6decd3c
pavel <pavel_ts_pavlov@outlook.com>
2021-10-03 07:32:48 +0300

    task 7 completed

---
+++ foodDelivery/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace foodDelivery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please isert quantity of chicken menus:");
+            int chickenMenu = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please isert quantity of fish menus:");
+            int fishMenu = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please isert quantity of vegeterian menus:");
+            int vegeterianMenu = int.Parse(Console.ReadLine());
+
+            double deliveryFee = 2.5;
+
+            double chickenOrder = chickenMenu * 10.35;
+            double fishOrder = fishMenu * 12.4;
+            double vegeterianOrder = vegeterianMenu * 8.15;
+            double order = chickenOrder + fishOrder + vegeterianOrder;
+            double desert = order * 0.2;
+
+            double finalOrder = deliveryFee + order + desert;
+
+            Console.WriteLine(finalOrder);
+
+        }
+    }
+}
