837ff6c40da9e3d6ff32c9978b1f01f26440c45e
pavel <pavel_ts_pavlov@outlook.com>
2021-10-07 22:18:36 +0300

    Homework2-task-1-and-2

---
+++ HW2Task1-seconds-sum/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HW2Task1_seconds_sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstTime = int.Parse(Console.ReadLine());
+            int secondTime = int.Parse(Console.ReadLine());
+            int thirdTime = int.Parse(Console.ReadLine());
+
+            int totalTime = firstTime + secondTime + thirdTime;
+
+            int minutesSum = totalTime / 60;
+            int secondsSum = totalTime % 60;
+
+            if (secondsSum < 10)
+            {
+                Console.WriteLine($"{minutesSum}:0{secondsSum}");
+            }
+            else {
+                Console.WriteLine($"{minutesSum}:{secondsSum}");
+            }
+        }
+    }
+}
---
+++ HW2Task2-bonus-score/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace HW2Task2_bonus_score
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startingScore = int.Parse(Console.ReadLine());
+            double bonusPoints = 0.0;
+            double finalScore = 0;
+
+            if (startingScore <= 100)
+            {
+                bonusPoints = 5;
+            }
+            else if (startingScore > 100 && startingScore <= 1000) {
+                
+                bonusPoints = startingScore * 0.2 ; 
+            }
+            else if (startingScore > 1000)
+            {
+                bonusPoints = startingScore * 0.1;
+            }
+
+            if (startingScore % 2==0)
+            {
+               
+                bonusPoints = bonusPoints + 1;
+             }
+            else if (startingScore % 10==5)
+            {
+               
+                bonusPoints = bonusPoints+2;
+            }
+            finalScore = startingScore+bonusPoints;
+            Console.WriteLine(bonusPoints);
+            Console.WriteLine(finalScore);
+
+        }
+    }
+}
