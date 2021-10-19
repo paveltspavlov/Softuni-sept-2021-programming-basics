3480553fc8213acf483950cf375263ba3993010c
pavel <pavel_ts_pavlov@outlook.com>
2021-10-19 23:25:42 +0300

    incomplete

--- HW2Task6-worldRecord/Program.cs
+++ HW2Task6-worldRecord/Program.cs
@@ -9,27 +9,32 @@ namespace HW2Task6_worldRecord
             double worldRecord = double.Parse(Console.ReadLine());
             double distance = double.Parse(Console.ReadLine());
             double timeInSeconds = double.Parse(Console.ReadLine());
-            double intermedeateTime = distance / 15;
+            double intermedeateTime;
             double result;
+            double result2;
             double output;
-
-            if (intermedeateTime > 14)
+            
+            if (distance > 14)
             {
-                timeInSeconds = timeInSeconds + (intermedeateTime * 12.5);
-                result = timeInSeconds * distance;
+                result2 = distance / 15;
+                intermedeateTime = distance *timeInSeconds;
+                
+                result = intermedeateTime + (result2 * 12.5);
+                //result = timeInSeconds * distance;
+                Math.Round(result,2);
             }
             else 
             {
                 result = timeInSeconds * distance;
+                Math.Round(result,2);
             }
-
             if (result < worldRecord)
             {
                 Console.WriteLine($"Yes, he succeeded! The new world record is {result:f2} seconds");
             }
             else 
             {
-                output = worldRecord - result;
+                output = result - worldRecord;
                 Console.WriteLine($"No, he failed! He was {output:f2} seconds slower.");
             }
         }
