d86de7c9b02b8506022e04665d702c3815f5b3c6
pavel <pavel_ts_pavlov@outlook.com>
2021-10-23 22:55:40 +0300

    homework 2 task 6 done

--- HW2Task6-worldRecord/Program.cs
+++ HW2Task6-worldRecord/Program.cs
@@ -9,29 +9,52 @@ namespace HW2Task6_worldRecord
             double worldRecord = double.Parse(Console.ReadLine());
             double distance = double.Parse(Console.ReadLine());
             double timeInSeconds = double.Parse(Console.ReadLine());
-            double intermedeateTime = distance / 15;
+            double intermedeateTime;
             double result;
+            double result2;
             double output;
+<<<<<<< HEAD
 
-            if (intermedeateTime > 14)
+            if (distance > 14)
             {
-                timeInSeconds = timeInSeconds + (intermedeateTime * 12.5);
-                result = timeInSeconds * distance;
-            }
-            else 
-            {
-                result = timeInSeconds * distance;
-            }
+                result2 = Math.Floor(distance / 15);
+                intermedeateTime = distance * timeInSeconds;
+                Math.Round(intermedeateTime, mode: MidpointRounding.AwayFromZero);
+                result = intermedeateTime + (result2 * 12.5);
+                //result = timeInSeconds * distance;
 
+=======
+            
+            if (distance > 14)
+            {
+                result2 = distance / 15;
+                intermedeateTime = distance *timeInSeconds;
+                
+                result = intermedeateTime + (result2 * 12.5);
+                //result = timeInSeconds * distance;
+                Math.Round(result,2);
+>>>>>>> 3480553fc8213acf483950cf375263ba3993010c
+            }
+            else
+            {
+                result = timeInSeconds * distance;
+                Math.Round(result,2);
+            }
             if (result < worldRecord)
             {
-                Console.WriteLine($"Yes, he succeeded! The new world record is {result:f2} seconds");
+                //output = Math.Floor(result);
+                Console.WriteLine($"Yes, he succeeded! The new world record is {result:f2} seconds.");
             }
-            else 
+            else
             {
-                output = worldRecord - result;
+<<<<<<< HEAD
+                //output = Math.Floor(result - worldRecord);
+=======
+>>>>>>> 3480553fc8213acf483950cf375263ba3993010c
+                output = result - worldRecord;
                 Console.WriteLine($"No, he failed! He was {output:f2} seconds slower.");
             }
         }
     }
 }
+ 
